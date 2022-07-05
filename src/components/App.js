b336a49df030df2e0098b506d0e3eb9515e2dbac
yashuzuwakia <bittukr.0000@gmail.com>
2022-07-05 14:41:31 +0530

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
+                <h1>IMAGE SEARCH</h1>
                 <SearchBar onSubmita={this.onSearchSubmit} />
                 <ImageList images={this.state.images} />
             </div>
